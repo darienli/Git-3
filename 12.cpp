c4c0b684a5026967b63b0408a77ce5e0570eee28
Daria Khusainova <dashahusainova@bk.ru>
2024-05-31 13:31:22 +0400

    Corrected mistake in result function

--- 12.cpp
+++ 12.cpp
@@ -1,74 +1,73 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-struct stack {
-    char letter;
-    stack* next;
+struct stack{
+    char inf;
+    stack *next;
 };
 
-void push(stack*& h, char x) {
-    stack* r = new stack;
-    r->letter = x;
+void push(stack *&h, char x){
+    stack *r = new stack;
+    r->inf = x;
     r->next = h;
     h = r;
 }
 
 char pop(stack*& h) {
-    char i = h->letter;
-    stack* r = h;
-    h = h->next;
-    delete r;
-    return i;
+	char i = h->inf;
+	stack* r = h;
+	h = h->next;
+	delete r;
+	return i;
 }
 
-void reverse(stack*& h) {
-    stack* head1 = NULL;
-    while (h)
-        push(head1, pop(h));
-    h = head1;
-}
-
-stack* result(stack*& h) {
-    stack* res = NULL;
-    stack* h1 = NULL;
-    char lastVowel = ' ';
-    while (h) {
-        char x = pop(h);
-        push(res, x);
-        if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
-            lastVowel = x;
-        while (h) {
-            char y = pop(h);
-            if (x != y)
-                push(h1, y);
-        }
-        reverse(h1);
-        h = h1;
-        h1 = NULL;
+void reverse(stack *&h){
+    stack *r = NULL;
+    char x;
+    while (h){
+        x = pop(h);
+        push(r, x);
     }
-    if (lastVowel != ' ')
-        push(res, '!');
-    reverse(res);
+    h = r;
+}
+
+stack* result(stack *&h){
+    stack *res = NULL;
+    int fl = 0;
+    while(h){
+        char x = pop(h);
+        if (x == 'e' || x == 'y' || x == 'u' || x == 'i' || x == 'o' || x == 'a'){
+            if (fl == 1){
+                push(res, x);
+            }
+            else{
+                push(res, '!');
+                push(res, x);
+                fl = 1;
+            }
+        }
+        else{
+            push(res, x);
+        }
+    }
+    //reverse(res);
     return res;
 }
 
-int main() {
-    int n;
-    cout << "n = ";
-    cin >> n;
-    stack* head = NULL;
+int main(){
+    int n; cout<<"n = "; cin >> n;
     char x;
-    for (int i = 0; i < n; i++) {
+    stack *h = NULL;
+    for (int i = 0; i < n; i++){
         cin >> x;
-        push(head, x);
+        push (h, x);
     }
-    reverse(head);
-    stack* res = result(head);
-    while (res)
+    stack* res = result(h);
+    while (res){
         cout << pop(res) << " ";
-
-    cout << endl;
-    return 0;
+    }
 }
+
+
+
 
